dc07dde943f7cbef0d2f342491fc3d8c50e5f74a
b4ldr <john.r.bond@gmail.com>
2013-06-25 22:17:40 +0200

    add atlas nagios lib

---
+++ atlas.py
@@ -0,0 +1,71 @@
+import time
+
+class Measurment: 
+    def __init__(self, probe_id, payload):    
+        self.probe_id = probe_id
+        self.payload = payload
+        self.check_time = self.payload[1]
+        self.msg = "Probe (%s): %s (%s)" 
+
+    def check_measurement_age(self, max_age, results):
+        min_time = time.time() - max_age
+        check_time_str = time.ctime(self.check_time)
+        if self.check_time < min_time:
+            results['error'].append(self.msg % (self.probe_id, "measurement to old", check_time_str))
+        else:
+            results['ok'].append(self.msg % (self.probe_id, "measurement fresh", check_time_str))
+
+    def check_string(self, check_string, measurment_string, check_type, results):
+        if check_string == measurment_string:
+            results['ok'].append(self.msg % (self.probe_id, check_type, measurment_string))
+        else:
+            results['error'].append(self.msg % (self.probe_id, check_type, measurment_string))
+
+    def check(self, nagios_args, results):             
+        if 'max_measurement_age' in nagios_args:
+            self.check_measurement_age(nagios_args['max_measurement_age'], results)
+
+class SSLcertMeasurment(Measurment):
+    def __init__(self, probe_id, payload):
+        #super(Measurment, self).__init__(payload)
+        Measurment.__init__(self, probe_id, payload)
+        self.common_name = self.payload[2][0][0]
+        self.expiry = time.mktime(time.strptime(self.payload[2][0][4],"%Y%m%d%H%M%SZ"))
+        self.sha1 = self.payload[2][0][5]
+
+    def check_expiry(self, warn_expiry, results):
+        current_time = time.time()
+        warn_time = current_time - (warn_expiry * 60 * 60 * 24)
+        expiry_str = time.ctime(self.expiry)
+        if self.expiry < current_time:
+            results['error'].append(self.msg % (self.probe_id, "certificate expierd", expiry_str))
+        elif self.expiry < warn_time:
+            results['warn'].append(self.msg % (self.probe_id, "certificate expires soon", expiry_str))
+        else:
+            results['ok'].append(self.msg % (self.probe_id, "certificate expiry good", expiry_str))
+
+    def check(self, nagios_args, results):
+        Measurment.check(self, nagios_args, results)
+        if 'sha1hash' in nagios_args:
+            self.check_string(nagios_args['sha1hash'], self.sha1, 'sha1hash', results)
+        if 'common_name' in nagios_args:
+            self.check_string(nagios_args['common_name'], self.common_name, 'cn', results)
+        if nagios_args['check_expiry'] and 'warn_expiry' in nagios_args:
+            self.check_expiry(nagios_args['warn_expiry'], results)
+
+class PingMeasurment(Measurment):
+    def __init__(self, probe_id, payload):
+        #super(Measurment, self).__init__(self, payload)
+        Measurment.__init__(self, probe_id, payload)
+        self.avg_rtt = self.payload[0]
+
+    def check_rtt(self, min_rtt, results):
+        if self.avg_rtt < min_rtt:
+            results['ok'].append(self.msg % (self.probe_id, "rtt", self.avg_rtt))
+        else:
+            results['error'].append(self.msg % (self.probe_id, "rtt", self.avg_rtt))
+
+    def check(self, nagios_args, results):
+        Measurment.check(self, nagios_args, results)
+        if 'min_rtt' in nagios_args:
+            self.check_rtt(nagios_args['min_rtt'], results)
